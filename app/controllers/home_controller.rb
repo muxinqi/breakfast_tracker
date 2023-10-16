1b49800b454a54b7ff856da74cd006b92a5b2de5
Xinqi Mu <hi@muxinqi.com>
2023-10-16 11:18:54 +0800

    Fix error: `undefined method finished?`

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,14 @@ class HomeController < ApplicationController
     @total_eggs = User.total_eggs
     @total_corn = User.total_corn
     last_cooking_record = CookingRecord.last
-    @in_progress_cooking_record = last_cooking_record unless last_cooking_record.finished?
+    @in_progress_cooking_record = if last_cooking_record.nil? || last_cooking_record.finished?
+                                    nil
+                                  else
+                                    last_cooking_record
+                                  end
     @cooking_records = CookingRecord.includes(:meals => :diner).order(created_at: :desc).limit(10)
     @total_eaten_meal = Meal.count
-    @total_eaten_eggs = CookingRecord.not(finished_at: nil).where("finished_at < ?", Time.now).sum(:egg_count)
-    @total_eaten_corn = CookingRecord.not(finished_at: nil).where("finished_at < ?", Time.now).sum(:corn_count)
+    @total_eaten_eggs = CookingRecord.where("finished_at < ?", Time.now).sum(:egg_count)
+    @total_eaten_corn = CookingRecord.where("finished_at < ?", Time.now).sum(:corn_count)
   end
 end
