737c6374a2ff3f63b73cbd2492be4718088cf4b7
Xinqi Mu <hi@muxinqi.com>
2023-10-22 13:30:19 +0800

    Ignoring error reporting in development environments

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
   config.dsn = 'https://5a64df421e68c61e90731aa9a4ca6aac@o1265120.ingest.sentry.io/4506058671063040'
+  config.enabled_environments = %w[production, staging]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
